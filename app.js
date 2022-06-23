baafde0317ed9ca1cd0e9d057f9e7df4ecb30912
balanasia <anabalasia@gmail.com>
2022-06-23 15:18:33 -0500

    Edit .gitignore file and add Procfile to be executed on Heroku

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //create the mongoose conneciton with todolistDB
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://anabalasia:LSO0lb7bE0tO0IXf@cluster0.duw2nrw.mongodb.net/todolistDB?retryWrites=true&w=majority");
 
 //create a mongoose Schema
 const itemSchema = {
@@ -173,6 +173,11 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port==null || port==""){
+  port = 3000;
+} 
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
